6451865532922f595c5c4f7bcfcc1b3bab788d55
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2024-03-04 13:23:53 -0600

    camera view transform

--- first_app.cpp
+++ first_app.cpp
@@ -26,6 +26,8 @@ namespace nate
 	{
 		SimpleRenderSystem simpleRenderSystem{ nateDevice, nateRenderer.getSwapChainRenderPass() };
         NateCamera camera{};
+        //camera.setViewDirection(glm::vec3(0.0f), glm::vec3(0.5f, 0.0f, 1.0f));
+        camera.setViewTarget(glm::vec3(-1.0f, -2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 2.5f));
 
 		while (!nateWindow.shouldClose())
 		{
--- simple_render_system.cpp
+++ simple_render_system.cpp
@@ -66,6 +66,9 @@ namespace nate
 	void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<NateGameObject>& gameObjects, const NateCamera& camera)
 	{
 		natePipeline->bind(commandBuffer);
+
+		auto projectionView = camera.getProjection() * camera.getView();
+
 		for (auto& obj : gameObjects)
 		{
 			obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
@@ -73,7 +76,7 @@ namespace nate
 
 			SimplePushConstantData push{};
 			push.color = obj.color;
-			push.transform = camera.getProjection() * obj.transform.mat4();
+			push.transform = projectionView * obj.transform.mat4();
 
 			vkCmdPushConstants(
 				commandBuffer,
