9071f2245b045beeb079da09d3dbd73f5a58e2a5
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2024-03-11 11:04:54 -0500

    start point lights

--- first_app.cpp
+++ first_app.cpp
@@ -19,7 +19,9 @@ namespace nate {
 
     struct GlobalUbo {
         glm::mat4 projectionView{ 1.0f };
-        glm::vec3 lightDirection = glm::vec3{ 1.0f, -3.0f, -1.0f };
+        glm::vec4 ambientLightColor{ 1.0f, 1.0f, 1.0f, 0.02f };
+        glm::vec3 lightPos{ -1.0f };
+        alignas(16) glm::vec4 lightColor{ 1.0f };
     };
 
 	FirstApp::FirstApp() {
@@ -67,6 +69,7 @@ namespace nate {
         camera.setViewTarget(glm::vec3(-1.0f, -2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 2.5f));
 
         auto viewerObject = NateGameObject::createGameObject();
+        viewerObject.transform.translation.z = -2.5f;
         KeyboardMovementController cameraController{};
 
         auto currentTime = std::chrono::high_resolution_clock::now();
@@ -82,7 +85,7 @@ namespace nate {
             camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
 
             float aspect = nateRenderer.getAspectRatio();
-            camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.01f, 10.0f);
+            camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.01f, 100.0f);
 
 			if (auto commandBuffer = nateRenderer.beginFrame()) {
                 int frameIndex = nateRenderer.getFrameIndex();
@@ -114,19 +117,27 @@ namespace nate {
         //std::shared_ptr<NateModel> nateModel = NateModel::createModelFromFile(nateDevice, "res/colored_cube.obj");
 		std::shared_ptr<NateModel> nateModel0 = NateModel::createModelFromFile(nateDevice, "res/smooth_vase.obj");
 		std::shared_ptr<NateModel> nateModel1 = NateModel::createModelFromFile(nateDevice, "res/flat_vase.obj");
+        std::shared_ptr<NateModel> quad = NateModel::createModelFromFile(nateDevice, "res/quad.obj");
 
         auto gameObject0 = NateGameObject::createGameObject();
         gameObject0.model = nateModel0;
-        gameObject0.transform.translation = { -0.5f, 0.5f, 2.5f };
+        gameObject0.transform.translation = { -0.5f, 0.5f, 0.0f };
         gameObject0.transform.scale = glm::vec3(3.0f);
 
         gameObjects.push_back(std::move(gameObject0));
 
         auto gameObject1 = NateGameObject::createGameObject();
         gameObject1.model = nateModel1;
-        gameObject1.transform.translation = { 0.5f, 0.5f, 2.5f };
+        gameObject1.transform.translation = { 0.5f, 0.5f, 0.0f };
         gameObject1.transform.scale = glm::vec3(3.0f);
 
         gameObjects.push_back(std::move(gameObject1));
+
+        auto floor = NateGameObject::createGameObject();
+        floor.model = quad;
+        floor.transform.translation = { 0.0f, 0.5f, 0.0f };
+        floor.transform.scale = glm::vec3(3.0f);
+
+        gameObjects.push_back(std::move(floor));
 	}
 }
